5497ade78e24a8968dad6a37aaa332e22fcd57c9
daniel_cohen <daniel1cohen13579@gmail.com>
2022-12-20 16:43:03 +0200

    Update class  Address.cs

---
+++ Models/Address.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System.Diagnostics;
+
+namespace Models;
+
+public class Address
+{
+    string? City { get; set; }
+    string? Street { get; set; }
+    string? HouseNumber { get; set; }
+
+    public Address (string? c , string? s, string? h)
+    {
+        City = c;
+        Street = s;
+        HouseNumber = h;
+    }
+
+    public override string ToString() => $@"
+          City - {City}
+          Street - {Street}
+          HouseNumber - {HouseNumber}";
+}
--- Models/Adrass.cs
+++
@@ -1,8 +0,0 @@
-﻿
-
-namespace Models;
-
-internal class Adrass
-{
-    
-}
