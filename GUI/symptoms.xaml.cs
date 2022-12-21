1b9ebcbe82928e4e9155268b273fca3018652724
daniel_cohen <daniel1cohen13579@gmail.com>
2022-12-21 02:39:40 +0200

    Edit windows

--- GUI/symptoms.xaml.cs
+++ GUI/symptoms.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Org.BouncyCastle.Asn1.Crmf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,37 @@ namespace GUI
         public symptoms()
         {
             InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+            string[] nameOfCheckBox = { "כאב מוכר "
+                                        ,"כאב לא מוכר - כאב פתאומי גם במנוחה",
+                                        "כאב לוחץ בבית החזה לא משתנה במגע / נשימה / שינוי תנוחה" ,
+                                         " כאבים דוקרים / קורעים בין השכמות או במרכז בית החזה"};
+            if (ChestPain.IsChecked == true)
+            {
+                ComboBox? ChildOfChestPain = new ComboBox() { MinWidth = 100, Name = "Child" };
+                
+                ChildOfChestPain.HorizontalAlignment = HorizontalAlignment.Center;
+                
+                for (int i = 0; i < 4; i++) { ChildOfChestPain.Items.Add($"{nameOfCheckBox[i]}"); }
+                options.Children.Add(ChildOfChestPain);
+                Grid.SetRow(ChildOfChestPain, 1); 
+            }
+
+            if(ChestPain.IsChecked == false)
+            {
+                
+                ComboBox? a1 = this.FindName("Child") as ComboBox;
+                options.Children.Remove(a1);
+
+            }
+               
+                
+                
+
         }
     }
 }
